03b135be943a02551752fd14bad2ac34b2f7ce38
xiaotiandada <952822399@qq.com>
2020-09-30 17:21:49 +0800

    perf: alert network tips

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -1,15 +1,24 @@
 import { JsonRpcProvider, WebSocketProvider } from '@ethersproject/providers';
 import networks from '@/helpers/networks.json';
+import i18n from "../i18n";
 
 export class Providers {
   public rpc?: JsonRpcProvider;
   public ws?: WebSocketProvider;
 
   async setNetwork(chainId) {
-    const rpcUrl: any = networks[chainId].rpcUrl;
-    this.rpc = new JsonRpcProvider(rpcUrl);
-    // const wsUrl: any = networks[chainId].wsUrl;
-    // this.ws = wsUrl ? new WebSocketProvider(wsUrl) : undefined;
+    try {
+      console.log('chainId', chainId)
+      if (chainId !== 1) {
+        alert(i18n.t('pleaseToggleWalletNetwork'))
+      }
+      const rpcUrl: any = networks[chainId].rpcUrl;
+      this.rpc = new JsonRpcProvider(rpcUrl);
+      // const wsUrl: any = networks[chainId].wsUrl;
+      // this.ws = wsUrl ? new WebSocketProvider(wsUrl) : undefined;
+    } catch (e) {
+      console.log('setNetwork error', e)
+    }
   }
 }
 
--- src/store/modules/ui.ts
+++ src/store/modules/ui.ts
@@ -16,14 +16,18 @@ const mutations = {
 const actions = {
   init: async ({ commit, dispatch }) => {
     commit('SET', { loading: true });
-    const connector = await Vue.prototype.$auth.getConnector();
-    if (connector) {
-      await dispatch('login', connector);
-    } else {
-      commit('HANDLE_CHAIN_CHANGED', 1);
+      try {
+        const connector = await Vue.prototype.$auth.getConnector();
+      if (connector) {
+        await dispatch('login', connector);
+      } else {
+        commit('HANDLE_CHAIN_CHANGED', 1);
+      }
+      await dispatch('getBlockNumber');
+      commit('SET', { loading: false, init: true });
+    } catch (e) {
+      console.log('e', e)
     }
-    await dispatch('getBlockNumber');
-    commit('SET', { loading: false, init: true });
   },
   loading: ({ commit }, payload) => {
     commit('SET', { loading: payload });
